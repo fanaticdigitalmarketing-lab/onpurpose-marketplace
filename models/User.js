95c93d6708458d6959787c850226a1a954642476
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-03-29 23:49:29 -0400

    Simplify User model with cleaner field definitions

--- models/User.js
+++ models/User.js
@@ -1,23 +1,22 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM('customer', 'provider'), defaultValue: 'customer' },
-}, {
-  hooks: {
-    beforeCreate: async (user) => {
-      user.password = await bcrypt.hash(user.password, 10);
-    }
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: DataTypes.STRING,
+  email: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  password: DataTypes.STRING,
+  role: {
+    type: DataTypes.ENUM('customer', 'provider'),
+    defaultValue: 'customer'
   }
 });
-
-User.prototype.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = User;
