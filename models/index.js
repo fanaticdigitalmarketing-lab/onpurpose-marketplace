271582421cfce27f901df1c8f170a2182aceebdc
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-03-29 23:49:06 -0400

    Simplify model relationships and clean up models index file

--- models/index.js
+++ models/index.js
@@ -1,20 +1,21 @@
 const sequelize = require('../config/database');
+
 const User = require('./User');
 const Service = require('./Service');
 const Booking = require('./Booking');
 
-User.hasMany(Service, { foreignKey: 'userId' });
-Service.belongsTo(User, { foreignKey: 'userId' });
+// Relationships
+User.hasMany(Booking);
+Booking.belongsTo(User);
 
-User.hasMany(Booking, { foreignKey: 'userId' });
-Booking.belongsTo(User, { foreignKey: 'userId' });
+Service.hasMany(Booking);
+Booking.belongsTo(Service);
 
-Service.hasMany(Booking, { foreignKey: 'serviceId' });
-Booking.belongsTo(Service, { foreignKey: 'serviceId' });
-
-module.exports = {
+const db = {
   sequelize,
   User,
   Service,
   Booking
 };
+
+module.exports = db;
