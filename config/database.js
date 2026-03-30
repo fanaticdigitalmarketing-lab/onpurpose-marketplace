03b5314f064a00e8e4fe8c9b17ffb1dd815efec7
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-03-29 23:45:25 -0400

    Update database config with Railway-specific connection string

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('your_railway_postgres_url', {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
-    },
-  },
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
