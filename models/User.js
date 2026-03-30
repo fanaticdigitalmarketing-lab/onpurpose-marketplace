fdcf9da19698d92be5d209f934168a1f3d5b8c3c
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-03-29 23:59:37 -0400

    Fix User model: add comparePassword method and bcrypt hashing hook

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
   id: {
@@ -17,6 +18,19 @@ const User = sequelize.define('User', {
     type: DataTypes.ENUM('customer', 'provider'),
     defaultValue: 'customer'
   }
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    }
+  }
 });
+
+// Password comparison method
+User.prototype.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = User;
