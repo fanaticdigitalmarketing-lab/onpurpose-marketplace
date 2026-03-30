0653fe59f1aca6c622495903dcac0ad5104389fd
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-03-29 23:49:50 -0400

    Simplify Service model with cleaner field definitions

--- models/Service.js
+++ models/Service.js
@@ -2,11 +2,14 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Service = sequelize.define('Service', {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  title: { type: DataTypes.STRING, allowNull: false },
-  description: { type: DataTypes.TEXT, allowNull: false },
-  price: { type: DataTypes.FLOAT, allowNull: false },
-  userId: { type: DataTypes.UUID, allowNull: false, references: { model: 'Users', key: 'id' } }
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  title: DataTypes.STRING,
+  description: DataTypes.TEXT,
+  price: DataTypes.FLOAT
 });
 
 module.exports = Service;
