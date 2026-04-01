54552ea22a6e26068200ec288c5b731132830f00
fanaticdigitalmarketing-lab <fanaticdigitalmarketing@gmail.com>
2026-04-01 10:12:45 -0400

    Trigger full redeploy - Railway and Netlify

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { sequelize } = require('./models');
-// Railway redeploy trigger - 2024-04-01-13-15
+// Railway redeploy trigger - 2024-04-01-13-58
 
 const authRoutes = require('./routes/auth');
 const authSimpleRoutes = require('./routes/auth-simple');
